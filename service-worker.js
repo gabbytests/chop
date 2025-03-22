e5fe64b090bfede814ac2cc5026da88905043842
havoschools <havoagenciesltd@gmail.com>
2025-03-22 00:29:50 +0000

    a

---
+++ service-worker.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "chawmp-cache-v1";
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/vendor/bootstrap/css/bootstrap.min.css",
+    "/vendor/slick/slick/slick.css",
+    "/vendor/slick/slick/slick-theme.css",
+    "/vendor/icons/feather.css",
+    "/css/style.css",
+    "/img/fav.png",
+    "/img/icon-192x192.png",
+    "/img/icon-512x512.png",
+    "/manifest.json",
+    "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js",
+    "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) return response;
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== "basic") {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => cache.put(event.request, responseToCache));
+                        return response;
+                    });
+            })
+            .catch(() => {
+                return caches.match("/index.html"); // Fallback to cached index
+            })
+    );
+});
